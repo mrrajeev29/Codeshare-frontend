3644ed4db6b3e328536292638b2111c431dc119f
Rajeev Ranjan <97228502+mrrajeev29@users.noreply.github.com>
2024-03-16 22:00:21 +0530

    Update Page.js

--- src/component/Pages/Page.js
+++ src/component/Pages/Page.js
@@ -23,9 +23,13 @@ window.onresize=function()
        document.getElementById("mNav1").style.display="flex";
         document.getElementById("mNav2").style.display="none";
         document.getElementById("i4").style.display="none";
-        document.getElementById("i5").style.display="flex";
+        document.getElementById("i5").style.display="none";
 
         document.getElementById("Pchild1").style.marginTop="5rem";
+    }
+    if(window.innerHeight<=500)
+    {
+        document.getElementById("i5").style.display="flex";
     }
 }
 
